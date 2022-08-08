024a96c5da2ef41f65d997b6c8fce62e7a48b6da
Xinyi Zhou <cintheswagster2015@gmail.com>
2022-08-08 01:33:37 -0500

    AB-16 Migrating enzyme test to use react testing library only.

--- src/AppNavbar.test.js
+++ src/AppNavbar.test.js
@@ -1,9 +1,30 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import AppNavbar from "./AppNavbar";
-import {shallow} from "enzyme";
+import {BrowserRouter} from "react-router-dom";
+
 
 test('render send email link', () => {
-  const appNavbarWrapper = shallow(<AppNavbar />);
-  expect(appNavbarWrapper.find('[href="mailto:xinyizhou1999@hotmail.com"]').length).toBe(1);
+  render(<AppNavbar />, {wrapper: BrowserRouter});
+    expect(screen.getByText('Send Email')).toBeInTheDocument();
+});
+
+test('render github link', () => {
+    render(<AppNavbar />, {wrapper: BrowserRouter});
+    expect(screen.getByText('GitHub')).toBeInTheDocument();
+});
+
+test('render send email link', () => {
+    render(<AppNavbar />, {wrapper: BrowserRouter});
+    // const linkDom = screen.getByRole('nav-link', { name: 'Send Email' });
+    const linkDom = screen.getByText(/Send Email/i);
+    // check the link location
+    expect(linkDom).toHaveAttribute("href", "mailto:xinyizhou1999@hotmail.com");
+});
+
+test('render github link', () => {
+    render(<AppNavbar />, {wrapper: BrowserRouter});
+    const linkDom = screen.getByText(/GitHub/i);
+    // check the link location
+    expect(linkDom).toHaveAttribute("href", "https://github.com/CyberSpaceExplorer/address-book-ui");
 });
